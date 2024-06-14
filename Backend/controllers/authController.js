40823187c84c8a7b4b10551873f3caad601cd7a9
Timothy <oludefiyinfoluwa06@gmail.com>
2024-06-14 09:33:49 +0100

    Registration logic complete

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -11,6 +11,29 @@ const createToken = (id) => {
     });
 }
 
+const signup = async (req, res) => {
+    const { email, displayName, username, password, dob } = req.body;
+
+    if (!email || !displayName || !username || !password || !dob) return res.status(400).json({ error: 'Input fields cannot be empty' });
+
+    if (!validator.isEmail(email)) return res.status(400).json({ error: 'Invalid email address' });
+
+    if (password.length < 8) return res.status(400).json({ error: 'Password length must be equal to or more than 8 characters' });
+
+    const emailExists = await User.findOne({ email });
+
+    if (emailExists) return res.status(400).json({ error: 'Email exists already' });
+
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+
+    const user = await User.create({ email, displayName, username, password: hash, dob });
+
+    const token = createToken(user._id);
+
+    return res.status(201).json({ message: 'Registration successful', user, token });
+}
+
 const login = async (req, res) => {
     const { email, password } = req.body;
 
@@ -24,7 +47,7 @@ const login = async (req, res) => {
 
     const token = createToken(user._id);
 
-    return res.status(200).json({ error: 'Login successful', user, token });
+    return res.status(200).json({ message: 'Login successful', user, token });
 }
 
 module.exports = {
--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,7 +1,8 @@
-const { login } = require('../controllers/authController');
+const { signup, login } = require('../controllers/authController');
 
 const router = require('express').Router();
 
+router.post('/signup', signup);
 router.post('/login', login);
 
 module.exports = router;
